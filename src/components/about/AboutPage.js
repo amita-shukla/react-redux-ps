e4f8ef69f77a09d1ec7d7e50b417f496ff2adaa4
amita-shukla <amitashukla0906@gmail.com>
2019-08-02 15:57:42 +0530

    add basic app structure using react components, routes, and 404 page

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>It feels good that I am finally getting a hang of this :D</p>
+  </div>
+);
+
+export default AboutPage;
