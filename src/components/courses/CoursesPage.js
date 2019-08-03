6e522128430f824666f330dbc34e028d49fe76e8
amita-shukla <amitashukla0906@gmail.com>
2019-08-03 19:00:31 +0530

    create simple add course form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,8 +2,37 @@ import React from "react";
 
 //creating this as a class component just for practice.
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {
+        title: ""
+      }
+    };
+
+    //else you get an error: "Cannot read property of undefined" when trying to type a course for add course
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    // this.setState({ course: course });
+    this.setState({ course }); // object shorthand syntax
+  }
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
