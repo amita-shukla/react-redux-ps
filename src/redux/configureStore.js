fb429415138740511031616d2cea0003b2af7883
amita-shukla <amitashukla0906@gmail.com>
2019-08-03 22:26:02 +0530

    add support for react dev tools

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import { reduxImmutableStateInvariant } from "redux-immutable-state-invariant";
 
 export default function configureStore(initialState) {
+  // add support for dev tools
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose; // unique global var
   return createStore(
     rootReducer,
     initialState,
-    // middleware so that we don't accidently mutate state
-    applyMiddleware(reduxImmutableStateInvariant())
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
   );
 }
